0a1724f6d37882b21d7e4c2dc0686436ae30d7be
Diego Alexander Delgado Maigual <diego@udenar.edu.co>
2020-09-09 18:05:34 -0500

    cabildo web inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="./css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <script src="./js/bootstrap.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <title>Sistema De Registro</title>
+        <link rel="stylesheet" href="./css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+    <div class="transparence">
+        <div class="menu-main">
+        <nav>
+        <strong>
+                <ul>
+                    <li>
+                        Inicio
+                    </li>
+                    <li>
+                        ¿Quienes somos?
+                    </li>
+                    <li>
+                        Contáctos
+                    </li>
+                    <li>
+                        Plan de vida
+                    </li>
+                    <li>
+                        Comunidad
+                    </li>
+
+                </ul>
+                </strong>
+            </nav>
+
+        </div>
+        <div class="container">
+        
+          <img class="logo" src="./imagen/isologo.png"/>
+          <img class="fondo" src="./imagen/header.svg"/>
+      
+        </div>
+        <div class="general">
+
+            <div class="space">
+                <img id="whipala" src="./imagen/whipala.svg">
+            </div>
+        </div>
+
+      <div class="carousel-container">
+          <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="./slides/1072x430campo.png" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+      </div>
+      <div class="footer-volcano">
+        <img id="img-footer" src="./imagen/footer.svg"/>
+      </div>
+      </div>    
+    </body>
+</html>
